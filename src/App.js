0100d88fa59c2e40dbb0db51588a64b5dfbbbbfb
pgsev77 <pgsev77@gmail.com>
2025-07-26 15:47:09 +0800

    Update App styles and content; add first page image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
+      {" "}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {" "}
+        <h1>欢迎来到我的后台管理系统</h1>
+        <p>这是我学习React的第一个项目！</p>{" "}
+        <p>当前时间：{new Date().toLocaleString()}</p>{" "}
+      </header>{" "}
     </div>
   );
 }
-
 export default App;
